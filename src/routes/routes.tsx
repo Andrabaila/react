2e87d5a205b2f4344ad4ed1946c739e7faff5941
Yury Andrabaila <androbailo@gmail.com>
2023-03-18 19:19:14 +0300

    feat: add redirect

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,18 +2,20 @@ import App from 'App';
 import PageAbout from 'pages/PageAbout';
 import PageNotFound from 'pages/PageNotFound';
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 
 export const routes = [
   {
     path: '/',
     element: <App />,
+    errorElement: <Navigate to="/404" replace />,
   },
   {
     path: 'about',
     element: <PageAbout />,
   },
   {
-    path: '*',
+    path: '/404',
     element: <PageNotFound />,
   },
 ];
